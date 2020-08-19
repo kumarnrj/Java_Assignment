1654f267339fc19710d036daa8e47383af1e8459
Neeraj Kumar <neeraj.neerajkumar11@gmail.com>
2020-08-20 00:59:04 +0530

    Question No.18 Program to reverse a queue

--- Assignment_2/RevQueue.java
+++ Assignment_2/RevQueue.java
@@ -1,4 +1,35 @@
 package com.Assignment_2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class RevQueue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(12);
+        queue.add(34);
+        queue.add(54);
+        queue.add(87);
+
+        System.out.println("The elements of the queue are:");
+        System.out.println(queue);
+
+        System.out.println("The elemets of the queue in reverse order");
+        Stack<Integer> st = new Stack<>();
+
+        while(!queue.isEmpty()){
+            int val =queue.peek();
+            queue.remove();
+            st.push(val);
+        }
+       
+        while(!st.empty()){
+            System.out.print(st.pop()+" ");
+        }
+    }
+
+
 }
